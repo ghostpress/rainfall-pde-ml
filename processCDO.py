580a203cf9f582ba7e9132fdc35aff80a14a3077
Lucia V <luciamvila@gmail.com>
2023-06-20 14:59:29 -0400

    Debugged select zaxis command

--- processCDO.py
+++ processCDO.py
@@ -16,5 +16,7 @@ for f in os.listdir(parent):
     sel_name = "sst_geo_" + f[-26:-18] + ".nc"
 
     # FIXME: error in selzaxisname
-    out = subprocess.run(["cdo", "-sellonlatbox,-18,-10,34,56", "-selzaxisname,sfc", "-selname,thetao", parent+f, outDir(sel_name)])
+    # FIXME: thetao variable does not have the "surface" z axis, it's only recorded starting from 0.5m below surface
+    # FIXME: find way to filter z axis by level=0.5 so not storing 5000+ m of data
+    out = subprocess.run(["cdo", "-sellonlatbox,-18,-10,34,56", "-selzaxis,2", "-selname,thetao", parent+f, outDir(sel_name)])
     print("Exit code: %d" % out.returncode)
