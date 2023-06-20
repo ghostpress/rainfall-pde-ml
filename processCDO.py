61f31025c5a0df5bc214f51218ba963438ba834e
Lucia V <luciamvila@gmail.com>
2023-06-20 19:51:43 -0400

    Tested code to filter z axis

--- processCDO.py
+++ processCDO.py
@@ -15,8 +15,5 @@ for f in os.listdir(parent):
 
     sel_name = "sst_geo_" + f[-26:-18] + ".nc"
 
-    # FIXME: error in selzaxisname
-    # FIXME: thetao variable does not have the "surface" z axis, it's only recorded starting from 0.5m below surface
-    # FIXME: find way to filter z axis by level=0.5 so not storing 5000+ m of data
-    out = subprocess.run(["cdo", "-sellonlatbox,-18,-10,34,56", "-selzaxis,2", "-selname,thetao", parent+f, outDir(sel_name)])
+    out = subprocess.run(["cdo", "-sellonlatbox,-18,-10,34,56", "-sellevel,0.494024992", "-selname,thetao", parent+f, outDir(sel_name)])
     print("Exit code: %d" % out.returncode)
