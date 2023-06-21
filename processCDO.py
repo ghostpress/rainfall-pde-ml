c2c8d523312b1ccf19ae955caeea7bcc0cc17a6f
Lucia V <luciamvila@gmail.com>
2023-06-20 21:57:14 -0400

    Processed all downloaded files

--- processCDO.py
+++ processCDO.py
@@ -5,15 +5,29 @@ def dataDir(x):
     return os.getcwd() + "/data/" +  x
 
 def outDir(x):
-    return dataDir("process/testCDO/" + x)
+    return dataDir("cdo_out/" + x)
 
 
-parent = dataDir("process/2022/04/")
-for f in os.listdir(parent):
+super_parent = dataDir("process")
+year_paths = os.listdir(super_parent)
 
-    print("Processing: %s" % f)
+print("Processing data in: %s" % super_parent)
 
-    sel_name = "sst_geo_" + f[-26:-18] + ".nc"
+for y in year_paths:
 
-    out = subprocess.run(["cdo", "-sellonlatbox,-18,-10,34,56", "-sellevel,0.494024992", "-selname,thetao", parent+f, outDir(sel_name)])
-    print("Exit code: %d" % out.returncode)
+    print("Descending into sub-directory: %s" % y)
+    
+    yf = super_parent + "/" + y
+    month_paths = os.listdir(yf)
+
+    for m in month_paths:
+
+        mf = yf + "/" + m
+        print("Descending into sub-directory: %s" % m)
+
+        for f in os.listdir(mf):
+            print("Processing: %s" % f)
+
+            sel_name = "sst_geo_" + f[-26:-18] + ".nc"
+            out = subprocess.run(["cdo", "-sellonlatbox,-18,-10,34,56", "-sellevel,0.494024992", "-selname,thetao", mf+"/"+f, outDir(sel_name)])
+            print("Exit code: %d" % out.returncode)
