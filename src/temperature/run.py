124d356e6d926015d241dd167a40e5e793369329
Lucia V <luciamvila@gmail.com>
2023-10-24 11:34:22 -0400

    Moved code from notebook to .py files

--- src/temperature/run.py
+++ src/temperature/run.py
@@ -3,10 +3,10 @@ import os
 import torch
 import torch.nn as nn
 
-from src.models import BezConv as BC
+from src.models.BezConv import BezConv as BC
 from src.dataloader import DataLoader
+from src.temperature.Experiment import Experiment
 import utils
-import Experiment
 
 device = utils.check_device()
 
@@ -24,10 +24,11 @@ DL.create_training()
 DL.create_validation()
 DL.create_test()
 
-net1 = BC(hist=4).to(device)
-# net1.to(device)
-optim = torch.optim.Adam(net1.parameters(), lr=1e-3)
+EL = utils.create_example_loader()
 
+# ~----- Experiment 1: no regularization for 100 epochs -----~
+net1 = BC(device=device, coeffs=[0,0,0], hist=4).to(device)
+optim1 = torch.optim.Adam(net1.parameters(), lr=1e-3)
 exp1 = Experiment.Experiment(name="BezenacSqErr_100",  # de Bezenac model, trained on err^2 Loss
                              trainset=DL.TrainLoader,  # data loaders: 1. training
                              valset=DL.ValLoader,      #               2. validation
@@ -35,7 +36,84 @@ exp1 = Experiment.Experiment(name="BezenacSqErr_100",  # de Bezenac model, train
                              model=net1,               # model
                              regloss=False,            # whether to regularize the training loss
                              test_loss=nn.MSELoss(reduction="mean"),  # loss function for testing
-                             optimizer=optim,
+                             optimizer=optim1,
                              outdir=path)
-
 exp1.run(epochs=100)
+
+# ~----- Experiment 2: magnitude regularization = -0.1 for 100 epochs -----~
+net2 = BC(device=device, coeffs=[-0.1,0,0], hist=4).to(device)
+optim2 = torch.optim.Adam(net2.parameters(), lr=1e-3)
+exp2 = Experiment.Experiment(name="BezenacRegSqErr_100",  # de Bezenac model, trained on err^2 Loss
+                             trainset=DL.TrainLoader,  # data loaders: 1. training
+                             valset=DL.ValLoader,      #               2. validation
+                             testset=DL.TestLoader,    #               3. testing
+                             model=net1,               # model
+                             regloss=True,             # whether to regularize the training loss
+                             test_loss=nn.MSELoss(reduction="mean"),  # loss function for testing
+                             optimizer=optim2,
+                             outdir=path)
+exp2.run(epochs=100)
+
+# ~----- Experiment 3: a range of regularization candidates, each for 50 epochs (lr=1e-3) ----~
+coeff_candidates = [-0.1, 0, 0.1, 1]
+
+for coeff in coeff_candidates:
+    # Magnitude
+    net = BC(device, [coeff, 0, 0], hist=4).to(device)
+    optim = torch.optim.Adam(net.parameters(), lr=1e-3)
+
+    exp = Experiment(name="BezConv_mag=" + str(coeff) + "_50",
+                     trainset=DL.TrainLoader,
+                     valset=DL.ValLoader,
+                     testset=DL.TestLoader,
+                     model=net,
+                     regloss=True,
+                     test_loss=nn.MSELoss(reduction="mean"),
+                     optimizer=optim,
+                     outdir=path)
+
+    print("Magnitude coefficient value:", coeff)
+    exp.run(epochs=50)
+    exp.visualize_examples(EL, "exampleImage")
+    exp.visualize_examples(DL.ValLoader, "validationImage")
+    exp.plot_loss("Performance in Training", "Epoch", ["train", "val"], "lossPlot")
+
+    # Divergence
+    net = BC(device, [0, coeff, 0], hist=4).to(device)
+    optim = torch.optim.Adam(net.parameters(), lr=1e-3)
+
+    exp = Experiment(name="BezConv_div=" + str(coeff) + "_50",
+                     trainset=DL.TrainLoader,
+                     valset=DL.ValLoader,
+                     testset=DL.TestLoader,
+                     model=net,
+                     regloss=True,
+                     test_loss=nn.MSELoss(reduction="mean"),
+                     optimizer=optim,
+                     outdir=path)
+
+    print("Divergence coefficient value:", coeff)
+    exp.run(epochs=50)
+    exp.visualize_examples(EL, "exampleImage")
+    exp.visualize_examples(DL.ValLoader, "validationImage")
+    exp.plot_loss("Performance in Training", "Epoch", ["train", "val"], "lossPlot")
+
+    net = BC(device, [coeff, 0, 0], hist=4).to(device)
+    optim = torch.optim.Adam(net.parameters(), lr=1e-3)
+
+    # Smoothness
+    exp = Experiment(name="BezConv_smo=" + str(coeff) + "_50",
+                     trainset=DL.TrainLoader,
+                     valset=DL.ValLoader,
+                     testset=DL.TestLoader,
+                     model=net,
+                     regloss=True,
+                     test_loss=nn.MSELoss(reduction="mean"),
+                     optimizer=optim,
+                     outdir=path)
+
+    print("Smoothness coefficient value:", coeff)
+    exp.run(epochs=50)
+    exp.visualize_examples(EL, "exampleImage")
+    exp.visualize_examples(DL.ValLoader, "validationImage")
+    exp.plot_loss("Performance in Training", "Epoch", ["train", "val"], "lossPlot")
--- src/temperature/utils.py
+++ src/temperature/utils.py
@@ -2,30 +2,6 @@ import torch
 import numpy as np
 
 
-def create_example_loader(X_files, y_files, batchsize=1, dtype=torch.FloatTensor):
-    X = []
-    y = []
-
-    for region in X_files:
-        region_X = []
-
-        for X_file in region:
-            x = np.load(X_file)
-            region_X.append(x)
-
-        X.append(region_X)
-
-    for region in y_files:
-        y.append(np.load(region))
-
-    X = torch.from_numpy(np.array(X)).type(dtype)
-    y = torch.from_numpy(np.array(y)).type(dtype)
-
-    loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X, y), batch_size=batchsize)
-
-    return loader
-
-
 def check_device():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using device:', device)
@@ -39,3 +15,57 @@ def check_device():
         print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), 'GB')
 
     return device
+
+
+def datadir(x):
+    return "/projectnb/labci/Lucia/data/" + x
+
+
+def create_example_loader(batchsize=1, dtype=torch.FloatTensor):
+    X = []
+    y = []
+
+    reg3_X = [datadir("sst_npy/sst_geo_20210605.nc_region_3.npy"),
+              datadir("sst_npy/sst_geo_20210606.nc_region_3.npy"),
+              datadir("sst_npy/sst_geo_20210607.nc_region_3.npy"),
+              datadir("sst_npy/sst_geo_20210608.nc_region_3.npy")]
+
+    reg22_X = [datadir("sst_npy/sst_geo_20210605.nc_region_22.npy"),
+               datadir("sst_npy/sst_geo_20210606.nc_region_22.npy"),
+               datadir("sst_npy/sst_geo_20210607.nc_region_22.npy"),
+               datadir("sst_npy/sst_geo_20210608.nc_region_22.npy")]
+
+    reg29_X = [datadir("sst_npy/sst_geo_20210605.nc_region_29.npy"),
+               datadir("sst_npy/sst_geo_20210606.nc_region_29.npy"),
+               datadir("sst_npy/sst_geo_20210607.nc_region_29.npy"),
+               datadir("sst_npy/sst_geo_20210608.nc_region_29.npy")]
+
+    reg44_X = [datadir("sst_npy/sst_geo_20210605.nc_region_44.npy"),
+               datadir("sst_npy/sst_geo_20210606.nc_region_44.npy"),
+               datadir("sst_npy/sst_geo_20210607.nc_region_44.npy"),
+               datadir("sst_npy/sst_geo_20210608.nc_region_44.npy")]
+
+    examples_X = [reg3_X, reg22_X, reg29_X, reg44_X]
+    examples_y = [datadir("sst_npy/sst_geo_20210609.nc_region_3.npy"),
+                  datadir("sst_npy/sst_geo_20210609.nc_region_22.npy"),
+                  datadir("sst_npy/sst_geo_20210609.nc_region_29.npy"),
+                  datadir("sst_npy/sst_geo_20210609.nc_region_44.npy")]
+
+    for region in examples_X:
+        region_X = []
+
+        for X_file in region:
+            x = np.load(X_file)
+            region_X.append(x)
+
+        X.append(region_X)
+
+    for region in examples_y:
+        y.append(np.load(region))
+
+    X = torch.from_numpy(np.array(X)).type(dtype)
+    y = torch.from_numpy(np.array(y)).type(dtype)
+
+    loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X, y), batch_size=batchsize)
+
+    return loader
