07a73273d703d4f290f63dbaa8f47ba8321aa524
Lucia V <luciamvila@gmail.com>
2024-02-28 12:49:44 -0500

    Added multiprocessing

--- src/processing/processERA5/download.py
+++ src/processing/processERA5/download.py
@@ -1,13 +1,14 @@
 import cdsapi
+import multiprocessing
 
 # ------------- SET UP -------------
 
 years = [y for y in range(1990, 2021, 1)]  # 1990-2020
-surface_vars = ['precipitation', '2m_dewpoint_temperature', '2m_temperature', 'convective_available_potential_energy',
-                'convective_inhibition', 'k_index', 'surface_pressure', 'total_cloud_cover',
-                'total_column_cloud_liquid_water', 'total_column_water_vapour',
+surface_vars = ['2m_dewpoint_temperature', '2m_temperature', 'convective_available_potential_energy',
+                'convective_inhibition', 'k_index', 'precipitation', 'specific_rain_water_content', 'surface_pressure',
+                'total_cloud_cover', 'total_column_cloud_liquid_water', 'total_column_water_vapour',
                 'vertically_integrated_moisture_divergence']  # streamfunction at 700 hPa
-level_vars = ['relative_humidity', 'specific_humidity', 'temperature', 'u_component_of_wind']
+level_vars = ['relative_humidity', 'specific_humidity', 'temperature', 'u_component_of_wind', 'vertical_velocity']
 levels = ['300', '500', '600', '700', '850', '925', '950']
 geo = [11.6, -3.8, 4.3, 1.8]  # Ghana region bounding box
 destination_path = '/home/lucia/projects/FORMES/rainfall-pde-ml/data/ERA5/new/'
@@ -16,8 +17,9 @@ destination_path = '/home/lucia/projects/FORMES/rainfall-pde-ml/data/ERA5/new/'
 
 c = cdsapi.Client()
 
+
 # Download surface variables:
-for var in surface_vars:
+def download_variable(var, levs):
 
     if var == 'precipitation':  # for precipitation, want daily accumulation -> all times
         times = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00',
@@ -31,43 +33,55 @@ for var in surface_vars:
         subset = list(map(str, subset))  # convert to strings for API
         fsuff = subset[0] + '-' + subset[len(subset)-1]  # get first, last year for filename
 
-        print(f"Requesting variable {var} for years {fsuff} on surface.")
+        if levels is None:
+            print(f"Requesting variable {var} for years {fsuff} on surface.")
 
-        c.retrieve('reanalysis-era5-single-levels',
-                   {'product_type': 'reanalysis',
-                    'format': 'netcdf',
-                    'area': geo,
-                    'variable': [var],
-                    'year': subset,
-                    'month': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-                    'day': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15',
-                            '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-                            '31'],
-                    'time': times,
-                    },
-                   destination_path + var + '_download_' + fsuff + '.nc')
+            c.retrieve('reanalysis-era5-single-levels',
+                       {'product_type': 'reanalysis',
+                        'format': 'netcdf',
+                        'area': geo,
+                        'variable': [var],
+                        'year': subset,
+                        'month': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+                        'day': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14',
+                                '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28',
+                                '29', '30', '31'],
+                        'time': times,
+                        },
+                       destination_path + var + '_download_' + fsuff + '.nc')
 
-# Download variables at pressure levels:
-for var in level_vars:
+        else:
+            print(f"Requesting variable {var} for years {fsuff} on pressure levels {levs} (hPa).")
 
-    for i in range(0, len(years), 10):  # 10-year intervals for download size limit
-        subset = years[i:i + 10]
-        subset = list(map(str, subset))  # convert to strings for API
-        fsuff = subset[0] + '-' + subset[len(subset)-1]  # get first, last year for filename
+            c.retrieve('reanalysis-era5-pressure-levels',
+                       {'product_type': 'reanalysis',
+                        'format': 'netcdf',
+                        'area': geo,
+                        'variable': [var],
+                        'pressure_level': levs,
+                        'year': subset,
+                        'month': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+                        'day': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14',
+                                '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28',
+                                '29', '30', '31'],
+                        'time': ['00:00', '06:00', '12:00', '18:00'],
+                        },
+                       destination_path + var + '_download_' + fsuff + '.nc')
 
-        print(f"Requesting variable {var} for years {fsuff} on pressure levels {levels} (hPa).")
 
-        c.retrieve('reanalysis-era5-pressure-levels',
-                   {'product_type': 'reanalysis',
-                    'format': 'netcdf',
-                    'area': geo,
-                    'variable': [var],
-                    'pressure_level': levels,
-                    'year': subset,
-                    'month': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-                    'day': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15',
-                            '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-                            '31'],
-                    'time': ['00:00', '06:00', '12:00', '18:00'],
-                    },
-                   destination_path + var + '_download_' + fsuff + '.nc')
+if __name__ == "__main__":
+    # Create a pool of processes for downloading single-level variables
+    pool_single = multiprocessing.Pool()
+
+    # Map the function to download_variable to the list of surface variables
+    pool_single.map(download_variable, surface_vars, None)
+
+    # Close the pool
+    pool_single.close()
+    pool_single.join()
+
+    # Repeat above for multi-level variables
+    pool_level = multiprocessing.Pool()
+    pool_level.map(download_variable, surface_vars, levels)
+    pool_level.close()
+    pool_level.join()
