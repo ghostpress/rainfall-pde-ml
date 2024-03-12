d0b788663853777e49128b86c672ad244bf4a968
Lucia Vilallonga <luciav@scc-zp2.scc.bu.edu>
2024-03-12 16:19:32 -0400

    Finished code for creating precipitation & ERA5 timeseries

--- src/processing/processERA5/download.py
+++ src/processing/processERA5/download.py
@@ -5,21 +5,20 @@ import multiprocessing
 
 years = [y for y in range(1990, 2021, 1)]  # 1990-2020
 surface_vars = ['2m_dewpoint_temperature', '2m_temperature', 'convective_available_potential_energy',
-                'convective_inhibition', 'k_index', 'precipitation', 'specific_rain_water_content', 'surface_pressure',
+                'convective_inhibition', 'k_index', 'surface_pressure',
                 'total_cloud_cover', 'total_column_cloud_liquid_water', 'total_column_water_vapour',
-                'vertically_integrated_moisture_divergence']  # streamfunction at 700 hPa
-level_vars = ['relative_humidity', 'specific_humidity', 'temperature', 'u_component_of_wind', 'vertical_velocity']
+                'vertically_integrated_moisture_divergence']  # add precipitation (hourly) when CDS API updated
+level_vars = ['relative_humidity', 'specific_humidity', 'specific_rain_water_content', 'temperature', 'u_component_of_wind', 'vertical_velocity']
 levels = ['300', '500', '600', '700', '850', '925', '950']
 geo = [11.6, -3.8, 4.3, 1.8]  # Ghana region bounding box
-destination_path = '/home/lucia/projects/FORMES/rainfall-pde-ml/data/ERA5/new/'
+destination_path = ''  # change as needed
 
 # ----------------------------------
 
 c = cdsapi.Client()
 
 
-# Download surface variables:
-def download_variable(var, levs):
+def download_variable(var, levs=None):
 
     if var == 'precipitation':  # for precipitation, want daily accumulation -> all times
         times = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00',
@@ -27,13 +26,13 @@ def download_variable(var, levs):
                  '22:00', '23:00']
     else:
         times = ['00:00', '06:00', '12:00', '18:00']  # all other variables, just 6h intervals
-
+    
     for i in range(0, len(years), 10):  # 10-year intervals for download size limit
         subset = years[i:i + 10]
         subset = list(map(str, subset))  # convert to strings for API
         fsuff = subset[0] + '-' + subset[len(subset)-1]  # get first, last year for filename
 
-        if levels is None:
+        if levs is None:
             print(f"Requesting variable {var} for years {fsuff} on surface.")
 
             c.retrieve('reanalysis-era5-single-levels',
@@ -68,20 +67,29 @@ def download_variable(var, levs):
                         },
                        destination_path + var + '_download_' + fsuff + '.nc')
 
+            
+def _unpack_args(list1, list2):
+    args = []
+    for l in list1:
+        args.append([l, list2])
+    return args
+
 
 if __name__ == "__main__":
     # Create a pool of processes for downloading single-level variables
     pool_single = multiprocessing.Pool()
 
     # Map the function to download_variable to the list of surface variables
-    pool_single.map(download_variable, surface_vars, None)
+    pool_single.map(download_variable, single_vars)
 
     # Close the pool
     pool_single.close()
     pool_single.join()
 
-    # Repeat above for multi-level variables
+    # Repeat above for multi-level variables (multiple map arguments)
+    level_args = _unpack_args(level_vars, levels)  
     pool_level = multiprocessing.Pool()
-    pool_level.map(download_variable, surface_vars, levels)
+    pool_level.starmap(download_variable, level_args)
+
     pool_level.close()
     pool_level.join()
